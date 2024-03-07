5047512a2ee3b9ea76c232feece5fc0aa7190480
PhoenixOfDoom <B.1.11.11@hotmail.com>
2024-03-07 16:09:27 +0200

    assignment 2

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,66 +1,76 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import cv2
+import random
 
-image_path = ""
+clown_image_path = "Assignment_2/Clown256B.bmp"
+barbara_image_path = "Assignment_2/Barbara.bmp"
 
-# def v_length_entropy(v):
-#
-#   symbol_counts = dict(collections.Counter(v))
-#
-#   huffman_tree = _build_huffman_tree(symbol_counts)
-#
-#   encoded_lengths = _get_encoded_lengths(huffman_tree, symbol_counts)
-#
-#   entropy = _calculate_entropy(symbol_counts)
-#   min_length = _calculate_min_length(entropy)
-#
-#   total_length = sum(encoded_lengths.values())
-#
-#   return total_length, min_length
 
-def length_entropy(S):
-    # ספירת כמות הופעות של כל איבר בווקטור
-    symbol_counts = {}
-    for symbol in S:
-        if symbol in symbol_counts:
-            symbol_counts[symbol] += 1
-        else:
-            symbol_counts[symbol] = 1
-
-    # חישוב האנטרופיה
+def entropy_length(vector):
     entropy = 0
-    total_symbols = len(S)
-    for count in symbol_counts.values():
-        probability = count / total_symbols
-        entropy += probability * math.log2(1 / probability)
-
-    # חישוב אורך הקוד המינימלי האפשרי על פי האנטרופיה
-    code_length = math.ceil(entropy)
-
-    return code_length
-
-
-def huffman_code_length(vector):
-    # Calculate the frequency of each element in the vector
     freq_dict = {}
-    for elem in vector:
-        if elem in freq_dict:
-            freq_dict[elem] += 1
-        else:
-            freq_dict[elem] = 1
+    total_elements = vector.shape[0] * vector.shape[1]
+    for row in range(vector.shape[0]):
+        for col in range(vector.shape[1]):
+            pixel_value = vector[row, col]
+            if pixel_value in freq_dict:
+                freq_dict[pixel_value] += 1
+            else:
+                freq_dict[pixel_value] = 1
 
-    # Calculate the entropy
-    total_elements = len(vector)
-    entropy = 0
-    for freq in freq_dict.values():
-        prob = freq / total_elements
-        entropy -= prob * math.log2(prob)
+    for key in freq_dict:
+        probability = freq_dict[key] / total_elements
+        entropy -= probability * math.log2(probability)
 
-    # Calculate the minimum code length using entropy
     code_length = math.ceil(entropy)
     return code_length
+
+
+def length_entropy(vector):
+    symbol_count = {}
+    total_symbols = len(vector)
+    for symbol in vector:
+        if symbol in symbol_count:
+            symbol_count[symbol] += 1
+        else:
+            symbol_count[symbol] = 1
+
+    entropy = 0
+    for count in symbol_count.values():
+        probability = count / total_symbols
+        entropy -= probability * math.log2(probability)
+
+    length = entropy / math.log2(len(symbol_count))
+
+    return length
+
+
+def show_graph_line(Input, Output):
+    # Plotting the results as a bar graph
+    plt.plot(Input, Output)
+    plt.xlabel('Inputs')
+    plt.ylabel('Outputs')
+    plt.title('Inputs versus Outputs')
+    plt.grid(True)
+    plt.show()
+    pass
 
 
 def Assignment_2():
+    barbara_image = cv2.imread(barbara_image_path, cv2.IMREAD_GRAYSCALE)
+    Clown_image = cv2.imread(clown_image_path, cv2.IMREAD_GRAYSCALE)
+
+    # data = np.array(img).reshape(-1)
+    encoded_length = entropy_length(Clown_image)
+
+    print(f" array length {encoded_length}")
+
+
+
+
+    width, height = barbara_image.shape[:2]
+    vector = [random.randint(0, 255) for _ in range(width * height)]
+
     pass
